af4d909fca57d9f667c2221e8a37963bbd7fcd94
anupo <anupo@10.44.39.4>
2020-02-16 20:52:04 -0500

    Push, pop and peek function added

--- StacksAndQueuesDS/src/stack/anupom/lab/practice/IntStackArray.java
+++ StacksAndQueuesDS/src/stack/anupom/lab/practice/IntStackArray.java
@@ -9,5 +9,65 @@ public class IntStackArray {//Stack Implementation on array
 		stack = new int[INITIAL_SIZE];
 		size = 0;
 	}
+	public int size() {
+		return size;
+	}
+	
+	public int capacity() {
+		return stack.length;
+	}
+	public int capacityRemaining() {
+		return stack.length-size;
+	}
+	
+	public void empty() {
+		for (int i = 0; i < stack.length; i++) {
+			stack[i]=0;
+		}
+	}
+	
+	private void compress() {
+		if(size<stack.length) {
+		int newCapacity=size;
+		int[] newStack= new int[newCapacity];
+		for (int i = 0; i < stack.length; i++) {
+			newStack[i]=stack[i];
+		}
+		stack=newStack;
+		}
+	}
+	private void doubleCapasity() {
+		int newCapacity=2*stack.length;
+		int[] newStack= new int[newCapacity];
+		for (int i = 0; i < stack.length; i++) {
+			newStack[i]=stack[i];
+		}
+		stack=newStack;
+	}
+	
+	
+	
+	public void push(int value) {
+		if(size==stack.length)
+			doubleCapasity();
+		stack[size]=value;
+		size++;
+	}
+	public int pop() {
+		if(size==0)
+			throw new ArrayIndexOutOfBoundsException();
+		int result=stack[size-1];
+		stack[size-1]=0;
+		size--;
+		return result;
+	}
+	
+	
+	public int peek() {
+		if(size==0)
+			throw new ArrayIndexOutOfBoundsException();
+		int result=stack[size-1];		
+		return result;
+	}
 
 }
