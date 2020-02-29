63b62d6a3678ec36e94929093d508c96e3faf896
anupo <anupo@DESKTOP-UP3QCI8>
2020-02-29 18:30:03 -0500

    Add delete (pop) & display functionality

--- StacksAndQueuesDS/src/stack/anupom/assignment4/q2/SingleLinkedStack.java
+++ StacksAndQueuesDS/src/stack/anupom/assignment4/q2/SingleLinkedStack.java
@@ -1,11 +1,12 @@
 package stack.anupom.assignment4.q2;
 
-//problem2Solution
+//problem2 ->Solution
 public class SingleLinkedStack {
+	
+	// Nested class 
 	private static class Node {
 		private int data;
 		private Node next;
-
 		private Node(int data) {
 			this.data = data;
 		}
@@ -17,13 +18,35 @@ public class SingleLinkedStack {
 		return top == null;
 	}
 
+	// To read data from stack
 	public int peek() {
 		return top.data;
 	}
-
+	// Add data from stack 
 	public void push(int data) {
 		Node node = new Node(data);
 		node.next = top;
 		top = node;
+	}
+	
+	// To Delete data from Stake
+	public int pop() {
+		int data = top.data;
+		top = top.next;
+		return data;
+	}
+	
+	// To display the linked list
+	public void display() {
+		Node current = top;
+		System.out.println();
+		while (current != null)
+		{
+			System.out.println(" | "+current.data + " | ");
+			System.out.println(" | ---- | ");
+			current = current.next;
+		}
+		System.out.println(" |______|");	
+		
 	}
 }
